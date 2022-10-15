e767b26f39916ccf8cb052d085386df0cdcc579e
Maxim Bazarov <bazaroffma@gmail.com>
2022-10-15 10:36:45 +0200

    bootstrap project (#1)

---
+++ Package.swift
@@ -0,0 +1,44 @@
+// swift-tools-version:5.7
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Decide package open source project
+//
+// Copyright (c) 2020-2022 Maxim Bazarov and the Decide package
+// open source project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+//
+import PackageDescription
+
+let package = Package(
+    name: "Decide",
+    platforms: [
+        .macOS(.v12),
+        .iOS(.v13),
+        .watchOS(.v8),
+        .tvOS(.v15),
+    ],
+    products: [
+        .library(name: "Decide", targets: ["Decide"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/MaximBazarov/Inject.git", from: "1.0.0")
+    ],
+    targets: [
+        .target(
+            name: "Decide",
+            dependencies: ["Inject"],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "Decide-Tests",
+            dependencies: ["Decide", "Inject"],
+            path: "Tests"
+        )
+    ]
+)
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Decide
-Application state management with automatic dependency graph, and Decision -> Effect -> Decision mutability.
+
+Effortless modular application state management with automatic dependency graph and Decision -> Effect -> Decision mutability.
+
+Decide focuses on the best performance while keeping it modular and simple for developers to use. 
+
+It solves application state management problems, a.k.a single source of trueth.
+
+## WORK IN PROGRESS
+It just started, so feel free to observe but keep in mind that it's not anywhere close to being stable in terms of API and performance.
