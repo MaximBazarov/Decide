7aa266b56e40a420606b5908eb4ba39876b4bf2b
Serj Agopian <serjagopian@gmail.com>
2023-09-02 10:31:37 +0200

    Update ObserveKeyed

--- Decide/Accessor/SwiftUI/ObserveKeyed.swift
+++ Decide/Accessor/SwiftUI/ObserveKeyed.swift
@@ -17,27 +17,27 @@ import SwiftUI
 /// **SwiftUI** property wrapper that provides read only access to the value by ``Property`` KeyPath and a Key on ``KeyedState`` from the view environment.
 @propertyWrapper
 @MainActor public struct ObserveKeyed<
-    I: Hashable,
-    S: KeyedState<I>,
+    Identifier: Hashable,
+    State: KeyedState<Identifier>,
     Value
 >: DynamicProperty {
 
     @SwiftUI.Environment(\.stateEnvironment) var environment
     @ObservedObject var observer = ObservedObjectWillChangeNotification()
 
-    let propertyKeyPath: KeyPath<S, Property<Value>>
+    let propertyKeyPath: KeyPath<State, Property<Value>>
 
-    public init(_ propertyKeyPath: KeyPath<S, Property<Value>>) {
+    public init(_ propertyKeyPath: KeyPath<State, Property<Value>>) {
         self.propertyKeyPath = propertyKeyPath
     }
 
     public init<P: PropertyModifier>(
-        _ propertyKeyPath: KeyPath<S, P>
+        _ propertyKeyPath: KeyPath<State, P>
     ) where P.Value == Value {
         self.propertyKeyPath = propertyKeyPath.appending(path: \.wrappedValue)
     }
 
-    public subscript(_ identifier: I) -> Value {
+    public subscript(_ identifier: Identifier) -> Value {
         get {
             environment.subscribe(Observer(observer), on: propertyKeyPath, at: identifier)
             return environment.getValue(propertyKeyPath, at: identifier)
